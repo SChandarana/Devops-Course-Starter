e668918b23baee76c26a48e10593193e33c1ccd6
Shivam Chandarana <shivam.chandarana@softwire.com>
2021-11-01 17:20:17 +0000

    Add todo list and ability to add items to list

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
+from todo_app.data.session_items import *
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', items=get_items())
+
+@app.route('/add', methods=['POST'])
+def addItem():
+    add_item(request.form.get('title'))
+    return redirect('/')
