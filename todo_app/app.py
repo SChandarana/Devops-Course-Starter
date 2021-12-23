1d275f56284525b075e02f38c593d30545e4c2e9
Shivam Chandarana <Shivam.Chandarana@softwire.com>
2021-12-23 16:54:47 +0000

    Add review markups

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, request
 
-from todo_app.data.session_items import *
+import todo_app.data.session_items as session_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -8,9 +8,9 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html', items=get_items())
+    return render_template('index.html', items=session_items.get_items())
 
 @app.route('/add', methods=['POST'])
 def addItem():
-    add_item(request.form.get('title'))
+    session_items.add_item(request.form.get('title'))
     return redirect('/')
